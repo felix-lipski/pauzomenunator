b051bf551a889557548cc6d794848ebce9423cd0
Felix Lipski <felix.lipski7@gmail.com>
2024-09-17 00:14:52 +0200

    fov works

---
+++ slider.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+var toggled = true
+
+func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	print(event)
+	if (event is InputEventMouseButton or (event is InputEventMouseMotion and event.button_mask == 1)):
+		var delta = (global_position - event_position).length()/0.12
+		print(delta)
+		$Handle.position.x = delta*0.12
+		get_node("/root/World/Player/Head/FirstPersonCameraReference/Camera3D").fov = 75 + (delta*25)
--- world.gd
+++ world.gd
@@ -4,7 +4,8 @@ var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Player/Head/PauseMenu.hide()
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,11 +13,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		if paused:
 			$Player/Head/PauseMenu.hide()
+			Engine.time_scale = 1
 			paused = false
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			paused = true
 			$Player/Head/PauseMenu.show()
+			Engine.time_scale = 0
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
 	pass
