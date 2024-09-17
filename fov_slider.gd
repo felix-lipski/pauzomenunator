35bdc60876ea50b643c37344a4daf1a36738c16f
Felix Lipski <felix.lipski7@gmail.com>
2024-09-17 14:38:28 +0200

    wip

---
+++ fov_slider.gd
@@ -0,0 +1,6 @@
+extends "res://slider.gd"  # Inherit from Slider.gd
+#
+## Override apply_value to implement specific behavior
+#func apply_value(delta: float) -> void:
+	#var camera = get_node("/root/World/Player/Head/FirstPersonCameraReference/Camera3D")
+	#camera.fov = 75 + (delta * 25)
