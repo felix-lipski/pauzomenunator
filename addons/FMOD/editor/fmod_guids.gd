5c4539a51774467f022059b50ce8eeccf818680f
gekon <megaog4r@gmail.com>
2024-09-18 20:41:06 +0200

    remove git ignore

---
+++ addons/FMOD/editor/fmod_guids.gd
@@ -0,0 +1,18 @@
+#
+   # fmod_guids.gd - FMOD Studio API
+
+   # Generated GUIDs for project 'Pauza.fspro'
+#
+
+class_name FMODGuids
+
+class Events:
+	const SFX_UI_CLICKS_KNOB = "{6395c575-a65c-4283-908a-add31b1fb409}"
+
+class Busses:
+	const MASTER_BUS = "{345f1e81-7834-44d0-88e1-398404cd4b4c}"
+	const REVERB = "{24814bac-725f-444b-b2cd-31d627882aeb}"
+
+class Banks:
+	const MASTER = "{2e3c3481-2a65-4b32-8ed1-9462b3dbbd0b}"
+
