1fbaf41df0cbf7b2789e874398a3258da75bc7c9
Felix Lipski <felix.lipski7@gmail.com>
2024-09-18 20:06:26 +0200

    scroller

--- button.gd
+++ button.gd
@@ -1,14 +1,16 @@
 extends Area3D
 @export var buttonsound: EventAsset
 var toggled = true
-@export var cos=1
 
+func on_press(toggled):
+	get_node("/root/World/Sun").shadow_enabled = toggled
 
 func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if (event.is_pressed()):
 		FMODRuntime.play_one_shot(buttonsound, self)
 		toggled = !toggled
-		get_node("/root/World/Sun").shadow_enabled = toggled
+		#get_node("/root/World/Sun").shadow_enabled = toggled
+		on_press(toggled)
 		
 		if toggled:
 			var new_color = Color(0, 1, 0, 1)
@@ -16,4 +18,3 @@ func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, 
 		else:
 			var new_color = Color(1, 0, 0, 1)
 			$Model.mesh.surface_get_material(0).albedo_color = new_color
-		print(toggled)
