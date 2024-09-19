0218fa3fa5db505d42bb3d560f8ad99af9b77045
Felix Lipski <felix.lipski7@gmail.com>
2024-09-19 16:24:47 +0200

    blocks

--- slider_sensitivity.gd
+++ slider_sensitivity.gd
@@ -1,5 +1,5 @@
 extends "res://slider.gd"
 
 func apply_value(delta: float) -> void:
-	var player = get_node("/root/World/Player")
+	var player = get_node("/root/LevelSwitcher/World/Player")
 	player.mouse_sensitivity = delta * 3
