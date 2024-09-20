58cb24ec7e65d8b0d8a653edba18ac28498c4ab5
Felix Lipski <felix.lipski7@gmail.com>
2024-09-20 14:48:42 +0200

    2 levels, chest, etc.

--- button.gd
+++ button.gd
@@ -11,10 +11,11 @@ func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, 
 		toggled = !toggled
 		#get_node("/root/World/Sun").shadow_enabled = toggled
 		on_press(toggled)
+		$ButtonBinary.toggle(toggled)
 		
-		if toggled:
-			var new_color = Color(0, 1, 0, 1)
-			$Model.mesh.surface_get_material(0).albedo_color = new_color
-		else:
-			var new_color = Color(1, 0, 0, 1)
-			$Model.mesh.surface_get_material(0).albedo_color = new_color
+		#if toggled:
+			#var new_color = Color(0, 1, 0, 1)
+			#$Model.mesh.surface_get_material(0).albedo_color = new_color
+		#else:
+			#var new_color = Color(1, 0, 0, 1)
+			#$Model.mesh.surface_get_material(0).albedo_color = new_color
