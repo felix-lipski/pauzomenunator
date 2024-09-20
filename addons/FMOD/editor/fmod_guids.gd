dfcaa9d1bc5521e78e819b5f695c54f0ce68305c
gekon <megaog4r@gmail.com>
2024-09-20 17:30:04 +0200

    sound v2

--- addons/FMOD/editor/fmod_guids.gd
+++ addons/FMOD/editor/fmod_guids.gd
@@ -7,7 +7,17 @@
 class_name FMODGuids
 
 class Events:
-	const SFX_UI_CLICKS_KNOB = "{6395c575-a65c-4283-908a-add31b1fb409}"
+	const AMBIENCE_AMBIENCE___LEVEL_1 = "{d1b24baf-17f3-4225-a715-da42e8bef424}"
+	const FOOTSTEPS_STEPS = "{ee7b76dc-02bc-4c0b-8f31-b7a10233f679}"
+	const SFX_CHEST_SFX_CHEST_NUMBER_SELECT = "{e509db4e-0462-4c91-88c1-c922e5ac8702}"
+	const SFX_CHEST_SFX_CHEST_UNLOCKED = "{47291653-3b3c-44a7-b12e-eda45c033768}"
+	const SFX_MODULE_SFX_MODULE_COLLECT = "{1a03b03d-5629-46d0-8a42-3432b7751646}"
+	const UI_MENU_IN_OUT_UI_MENU_EVENTS = "{9c8d5fd1-ae41-4035-8e65-566e5286e525}"
+	const UI_MODULE_CLICKS_UI_MODULE_CLICK = "{648a4af2-9c79-476c-a64c-03bf4e3b80b7}"
+	const UI_MODULE_CLICKS_UI_MODULE_CLICK_2 = "{a089bb2a-c113-4eeb-9ac9-fee48fddf67a}"
+	const UI_MODULE_CLICKS_UI_MODULE_CLICKS_LEVER = "{45fcaceb-0cfe-4013-af14-1ded9da32beb}"
+	const UI_MODULE_FOV_UI_MODULE_FOV = "{4a85fff1-6426-4126-8d26-88d03de8ce45}"
+	const UI_MODULE_KEY_SELECT_UI_MODULE_KEY_SELECTED = "{24beffeb-1cd9-4388-a9dd-8e9cd8ae52ac}"
 
 class Busses:
 	const MASTER_BUS = "{345f1e81-7834-44d0-88e1-398404cd4b4c}"
@@ -15,4 +25,12 @@ class Busses:
 
 class Banks:
 	const MASTER = "{2e3c3481-2a65-4b32-8ed1-9462b3dbbd0b}"
+
+class Parameters:
+	const CARPET = "{372e57e7-9784-42d3-b3da-b59f77451d08}"
+	const CONCRETE = "{03fe13ea-c0cb-47cb-9e42-073dab676b5f}"
+	const MENU_CLOSE = "{e5fa9f2a-0cf3-4a9a-a03d-d1b884a3b2a8}"
+	const MENU_OPEN = "{6ec96cfd-dc0e-4731-9cdd-17d0981497b8}"
+	const OKNO_ODFILTROWANIE = "{222e8fa0-a107-4708-b705-e78d0e3d0c10}"
+	const SPEED = "{a5bf7515-8489-4481-8cea-aaced817b318}"
 
