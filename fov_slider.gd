4b0e34e5034d20c257575ea99e9d436e0511cc06
gekon <megaog4r@gmail.com>
2024-09-20 17:30:04 +0200

    sound v2

--- fov_slider.gd
+++ fov_slider.gd
@@ -1,5 +1,6 @@
 extends "res://slider.gd"
-
+@export var fov: EventAsset
 func apply_value(delta: float) -> void:
 	var camera = get_node("/root/LevelSwitcher/World/Player/Head/FirstPersonCameraReference/Camera3D")
 	camera.fov = 75 + (delta * 25)
+	FMODRuntime.play_one_shot(fov, self)
