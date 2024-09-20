c17c22a78871c57368d0bb32bbdc2b8651a6f473
gekon <megaog4r@gmail.com>
2024-09-20 19:10:14 +0200

    sound v3

--- password_number.gd
+++ password_number.gd
@@ -1,10 +1,10 @@
 extends Area3D
-
+@export var chest_key_select: EventAsset
 var number = 0
 
 func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if (event.is_pressed()):
 		number = (number + 1) % 10
-
+		FMODRuntime.play_one_shot(chest_key_select, self) 
 func _process(delta: float) -> void:
 	$Model/Number.text = String.num(number)
