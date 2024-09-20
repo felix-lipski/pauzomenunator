855edc84a713f84ba3281434dc943be9fd335943
Felix Lipski <felix.lipski7@gmail.com>
2024-09-20 18:27:39 +0200

    restructure

--- fov_slider.gd
+++ fov_slider.gd
@@ -2,4 +2,4 @@ extends "res://slider.gd"
 
 func apply_value(delta: float) -> void:
 	var camera = get_node("/root/LevelSwitcher/World/Player/Head/FirstPersonCameraReference/Camera3D")
-	camera.fov = 75 + (delta * 25)
+	camera.fov = 50 + (delta * 50)
