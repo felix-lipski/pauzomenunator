baa58d0e03c5629dc533242a79da5259ca1eeca9
Felix Lipski <felix.lipski7@gmail.com>
2024-09-20 19:13:56 +0200

    wip

--- chest.gd
+++ chest.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var initial = [0,0,0]
 @export var password = [0,0,0]
 
+var locked = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +17,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if $Password/Key1.number == password[0] and $Password/Key2.number == password[1] and $Password/Key3.number == password[2]:
 		$Lid.rotation.x = -deg_to_rad(20)
+		if locked:
+			print("lkjkj")
+			# tutaj dajesz dźwiek
+			locked = false
